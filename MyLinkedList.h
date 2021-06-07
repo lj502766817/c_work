e155b9111ff748c3f20bdce51a671a4041fa5ce7
lj502766817 <502766817@qq.com>
2021-06-07 18:57:02 +0800

    Update MyLinkedList.h

--- MyLinkedList.h
+++ MyLinkedList.h
@@ -25,4 +25,23 @@ typedef struct DNode{
  */
 LinkedList deleteValueX(LinkedList L, int X);
 
+/**
+ * 在带头结点的单链表L中，删除所有值为x的结点，并释放其空间，假设值为x的结点不唯一，试编写算法以实现上述操作
+ * @param L
+ * @param x
+ */
+void removeValueX(LinkedList L, int x);
+
+/**
+ * 设L为带头结点的单链表，编写算法实现从尾到头反向输出每个结点的值
+ * @param L
+ */
+void printListReverse(LinkedList L);
+
+/**
+ * 试编写在带头结点的单链表L中删除一个最小值结点的高效算法（假设最小值结点是唯一的)
+ * @param L
+ */
+void deleteMin(LinkedList L);
+
 #endif //DEMO1_MYLINKEDLIST_H
