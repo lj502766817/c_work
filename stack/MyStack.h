4fdf147dcaaccc2fb1410218a30c24452987d4e2
lj502766817 <502766817@qq.com>
2021-06-20 22:30:16 +0800

    栈算法题

---
+++ stack/MyStack.h
@@ -0,0 +1,17 @@
+/*
+ * @Author: lijia
+ * @Date: 2021-06-20 22:25:29
+ * @LastEditTime: 2021-06-20 22:27:20
+ * @LastEditors: Please set LastEditors
+ * @Description: 栈算法题
+ * @FilePath: \c_work\stack\MyStack.h
+ */
+
+#include <stdbool.h>
+
+#ifndef DEMO1_MYSTACK_H
+#define DEMO1_MYSTACK_H
+
+
+
+#endif //DEMO1_MYSTACK_H
---
+++ stack/StackDemo.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: lijia
+ * @Date: 2021-06-20 22:25:17
+ * @LastEditTime: 2021-06-20 22:28:48
+ * @LastEditors: Please set LastEditors
+ * @Description: 栈算法题
+ * @FilePath: \c_work\stack\StackDemo.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "MyStack.h"
+
+int main(){
+    printf("hello Stack\n");
+}
