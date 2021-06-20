3cf5c77581084d3d7fe03e9f52f510af579176a1
lj502766817 <502766817@qq.com>
2021-06-20 21:22:34 +0800

    KMP待wancheng

--- other/KMP_demo.c
+++ other/KMP_demo.c
@@ -1,7 +1,7 @@
 /*
  * @Author: lijia
  * @Date: 2021-06-14 14:48:33
- * @LastEditTime: 2021-06-20 21:21:28
+ * @LastEditTime: 2021-06-20 21:22:17
  * @LastEditors: Please set LastEditors
  * @Description: 字符串匹配 KMP算法
  * @FilePath: \c_work\other\KMP_demo.c
@@ -11,6 +11,6 @@
 
 int main(){
     printf("KMP算法!\n");
-    //TODO:
+    //TODO: 
 }
 
