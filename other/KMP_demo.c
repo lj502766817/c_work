ecdabaf406f74d60f788c3f08e3987132fb21f98
lj502766817 <502766817@qq.com>
2021-06-20 21:21:41 +0800

    KMP算法

--- other/KMP_demo.c
+++ other/KMP_demo.c
@@ -1,11 +1,16 @@
-//
-// 字符串匹配 KMP算法
-// Created by 李佳 on 2021/6/14 0014.
-//
+/*
+ * @Author: lijia
+ * @Date: 2021-06-14 14:48:33
+ * @LastEditTime: 2021-06-20 21:21:28
+ * @LastEditors: Please set LastEditors
+ * @Description: 字符串匹配 KMP算法
+ * @FilePath: \c_work\other\KMP_demo.c
+ */
 
 #include <stdio.h>
 
 int main(){
     printf("KMP算法!\n");
+    //TODO:
 }
 
