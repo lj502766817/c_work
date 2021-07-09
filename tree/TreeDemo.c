3df2ed08abfd232de15d496cd36dac7a88c1e43f
lj502766817 <502766817@qq.com>
2021-07-09 17:08:23 +0800

    Update TreeDemo.c

--- tree/TreeDemo.c
+++ tree/TreeDemo.c
@@ -2,12 +2,16 @@
  * @description: 
  * @Date: 2021-07-05 14:45:09
  * @LastEditors: lijia
- * @LastEditTime: 2021-07-05 16:22:26
+ * @LastEditTime: 2021-07-09 16:56:35
  * @FilePath: \c_work\demo1\tree\TreeDemo.c
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "MyTree.h"
+
+//数组初始值长度
+const int DEFAULT_LENGTH = 10;
 
 /**
  * 首先在一个二叉树中,任意两个非根结点的结点是一定有公共祖先节点的.又可知一个结点i的父节点是i/2取整.
@@ -28,8 +32,36 @@ int getCommonParent(int i,int j){
     return i;
 }
 
+void subTraversal(BiTree t, int cnt){
+    printf("subTraversal:\t");
+    int index=-1;
+    //用来存放需要回溯的结点
+    BiTree arr[cnt];
+    //表示正在访问的结点
+    BiTree p=t;
+    //表示刚访问的结点
+    BiTree r=NULL;
+    while (p||index>=0) 
+    {   
+        //先一直走到最左边
+        if (p->lChild!=NULL)
+        {
+            arr[++index]=p;
+            p=p->lChild;
+        }
+        else
+        {
+            //
+        }
+        
+    }
+    
+}
+
 int main(){
     printf("hello tree.\n");
-    int result = getCommonParent(4,5);
-    printf("common parent is:%d\n",result);
+    // int result = getCommonParent(4,5);
+    // printf("common parent is:%d\n",result);
+    BiTree t;
+    subTraversal(t,10);
 }
