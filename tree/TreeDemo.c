a86637fdffcc5fd6588102c1b8576629bf5fc752
lj502766817 <502766817@qq.com>
2021-07-12 18:52:53 +0800

    Update TreeDemo.c

--- tree/TreeDemo.c
+++ tree/TreeDemo.c
@@ -2,7 +2,7 @@
  * @description: 
  * @Date: 2021-07-05 14:45:09
  * @LastEditors: lijia
- * @LastEditTime: 2021-07-09 16:56:35
+ * @LastEditTime: 2021-07-12 16:20:33
  * @FilePath: \c_work\demo1\tree\TreeDemo.c
  */
 
@@ -34,6 +34,7 @@ int getCommonParent(int i,int j){
 
 void subTraversal(BiTree t, int cnt){
     printf("subTraversal:\t");
+    //指向当前栈顶
     int index=-1;
     //用来存放需要回溯的结点
     BiTree arr[cnt];
@@ -44,16 +45,66 @@ void subTraversal(BiTree t, int cnt){
     while (p||index>=0) 
     {   
         //先一直走到最左边
-        if (p->lChild!=NULL)
+        if (p!=NULL)
         {
             arr[++index]=p;
             p=p->lChild;
         }
         else
         {
-            //
+            //获得栈顶结点
+            p=arr[index];
+            //右结点存在并且右结点没有被访问过,往右走
+            if (p->rChild&&p->rChild!=r)
+            {
+                p = p->rChild;
+                arr[++index]=p;
+                //继续往左走
+                p = p->lChild;
+            }
+            //右边没有,或者已经处理完成,输出该节点,并设置已访问指针,将p重置为NULL来获取下一个栈顶的结点
+            else
+            {   
+                index--;
+                printf("%c\t",p->value);
+                r=p;
+                p=NULL;
+            } 
         }
-        
+    } 
+}
+
+void seqTraversal(BiTree t, int cnt){
+    BiTree queue[cnt];
+    char stack[cnt];
+    int front=0,rear=0,top=-1;
+    queue[rear]=t;
+    rear = (rear+1+cnt)%cnt;
+    //使用队列进行正向的层序遍历
+    while (front!=rear)
+    {
+        BiTree node = queue[front];
+        front = (front+1+cnt)%cnt;
+        //值放入栈中
+        stack[++top]=node->value;
+        //放入左子结点
+        if (node->lChild!=NULL)
+        {
+            queue[rear]=node->lChild;
+            rear = (rear+1+cnt)%cnt;
+        }
+        //放入右子结点
+        if (node->rChild!=NULL)
+        {
+            queue[rear]=node->rChild;
+            rear = (rear+1+cnt)%cnt;
+        }
+    }
+    
+    printf("SeqTraversal:");
+    for (int i = top; i >=0; i--)
+    {
+        printf("%c\t",stack[i]);
     }
     
 }
@@ -62,6 +113,7 @@ int main(){
     printf("hello tree.\n");
     // int result = getCommonParent(4,5);
     // printf("common parent is:%d\n",result);
-    BiTree t;
-    subTraversal(t,10);
+    BiTree t = creatTree();
+    // subTraversal(t,10);
+    seqTraversal(t,10);
 }
