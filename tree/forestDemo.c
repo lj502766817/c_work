78e3f3636eb9f5e9776f8a8d9a9392d516e46cb8
lj502766817 <502766817@qq.com>
2021-07-19 19:51:45 +0800

    forest

---
+++ tree/forestDemo.c
@@ -0,0 +1,14 @@
+/*
+ * @description: 
+ * @Date: 2021-07-19 19:50:16
+ * @LastEditors: lijia
+ * @LastEditTime: 2021-07-19 19:51:00
+ * @FilePath: \c_work\demo1\tree\forestDemo.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("hello forest.\n");
+}
