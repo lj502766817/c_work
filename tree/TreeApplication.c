47bcc7aa1419a6126c9419f4a18fcc1dfafab412
lj502766817 <502766817@qq.com>
2021-07-27 19:11:07 +0800

    树搞完了

--- tree/TreeApplication.c
+++ tree/TreeApplication.c
@@ -1,10 +1,10 @@
 /*
  * @Author: your name
  * @Date: 2021-07-26 15:12:10
- * @LastEditTime: 2021-07-26 18:15:05
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-07-27 16:54:19
+ * @LastEditors: lijia
  * @Description: In User Settings Edit
- * @FilePath: \c_work\tree\TreeApplication.c
+ * @FilePath: \c_work\demo1\tree\TreeApplication.c
  */
 
 #include <stdio.h>
@@ -13,9 +13,14 @@
 
 typedef struct BSTNode
 {
-    int data;
+    int data,count;
     struct BSTNode *lChild, *rChild;
 } BSTNode, *BSTree;
+
+/**
+ * 根据先序和中序构造树
+*/
+BSTree buildTree(int pre[], int preLen, int mid[], int midLen);
 
 /**
  * 二叉排序树插入
@@ -31,6 +36,126 @@ bool checkBst(BSTree);
  * 查看结点在数的第几层
 */
 int checkTier(BSTree, int);
+
+/**
+ * 判断一个树是否是平衡二叉树
+*/
+bool checkBalance(BSTree, int *);
+
+/**
+ * 找到二叉排序树的最大最小关键字
+*/
+void findMaxAndMin(BSTree);
+
+/**
+ * 找到二叉排序树中所有不大于k的关键字,从大到小输出
+ * ------------------------------------
+ * 也可以正常的中序遍历然后用辅助栈进行逆向输出
+*/
+void findByK(BSTree, int);
+
+BSTree findIndexK(BSTree, int);
+
+BSTree findIndexK(BSTree tree, int k)
+{   
+    if (tree==NULL)
+    {
+        return NULL;
+    }
+    
+    int lCnt=0,rCnt=0;
+    if (tree->lChild!=NULL)
+    {
+        lCnt=tree->lChild->count+1;
+    }
+    if (tree->rChild!=NULL)
+    {
+        rCnt = tree->rChild->count+1;
+    }
+    if (k==lCnt+1)
+    {
+        printf("the node is %d.\n",tree->data);
+        return tree;
+    }
+    //k在左子树上
+    if (k<lCnt+1)
+    {
+        return findIndexK(tree->lChild,k);
+    }
+    //k在右子树上
+    else
+    {
+        return findIndexK(tree->rChild,k-lCnt-1);
+    }
+    
+    return NULL;
+}
+
+void findByK(BSTree tree, int k)
+{
+    //进行先右子树,再根节点,再左子树的中序遍历,进行从大到小的输出
+    if (tree==NULL)
+    {
+        return;
+    }
+    if (tree->rChild!=NULL)
+    {
+        findByK(tree->rChild,k);
+    }
+    if (tree->data>=k)
+    {
+        printf("%d\t",tree->data);
+    }
+    if (tree->lChild!=NULL)
+    {
+        findByK(tree->lChild,k);
+    }    
+}
+
+void findMaxAndMin(BSTree tree)
+{
+    BSTree p = tree;
+    int max,min;
+    while (p)
+    {
+        min= p->data;
+        p = p->lChild;
+    }
+    p = tree;
+    while (p)
+    {
+        max = p->data;
+        p = p->rChild;
+    }
+    
+    printf("max is:%d,min is:%d.\n",max,min);
+}
+
+bool checkBalance(BSTree tree, int *height)
+{
+    //平衡二叉树的左右子树都是平衡二叉树,并且左右子树高度差不大于1;
+    if (tree == NULL)
+    {
+        *height = 0;
+        return true;
+    }
+
+    int hL, hR;
+    if (!checkBalance(tree->lChild, &hL) || !checkBalance(tree->rChild, &hR))
+    {
+        return false;
+    }
+    if ((hL > hR ? hL-hR : hR-hL)<=1)
+    {
+        *height = hL > hR ? hL + 1 : hR + 1;
+    }
+    else
+    {
+        return false;
+    }
+    
+    return true;
+}
 
 int checkTier(BSTree tree, int value)
 {
@@ -49,7 +174,7 @@ int checkTier(BSTree tree, int value)
         p = p->data < value ? p->rChild : p->lChild;
     }
 
-    printf("there is no node %d.\n",value);
+    printf("there is no node %d.\n", value);
     return 0;
 }
 
@@ -101,6 +226,7 @@ BSTree insertBst(BSTree tree, int value)
     {
         BSTree bst = malloc(sizeof(BSTNode));
         bst->data = value;
+        bst->count = 0;
         bst->lChild = NULL;
         bst->rChild = NULL;
         return bst;
@@ -115,6 +241,7 @@ BSTree insertBst(BSTree tree, int value)
         {
             BSTree bst = malloc(sizeof(BSTNode));
             bst->data = value;
+            bst->count = 0;
             bst->lChild = NULL;
             bst->rChild = NULL;
             tree->lChild = bst;
@@ -131,17 +258,82 @@ BSTree insertBst(BSTree tree, int value)
         {
             BSTree bst = malloc(sizeof(BSTNode));
             bst->data = value;
+            bst->count = 0;
             bst->lChild = NULL;
             bst->rChild = NULL;
             tree->rChild = bst;
         }
     }
-
+    tree->count+=1;
     return tree;
+}
+
+BSTree buildTree(int pre[], int preLen, int mid[], int midLen)
+{
+    BSTree t = malloc(sizeof(BSTNode));
+    int head = pre[0];
+    t->data = head;
+    t->lChild = NULL;
+    t->rChild = NULL;
+    //如果先序和中序都是一个结点,直接返回
+    if (preLen == 1 || midLen == 1)
+    {
+        return t;
+    }
+
+    //找到父节点在中序遍历中的位置
+    int index;
+    for (int i = 0; i < midLen; i++)
+    {
+        if (t->data == mid[i])
+        {
+            index = i;
+            break;
+        }
+    }
+    //生成左子树
+    int leftNum = index;
+    if (leftNum > 0)
+    {
+        int midLeft[leftNum], preLeft[leftNum];
+        //处理中序遍历
+        for (int i = 0; i < index; i++)
+        {
+            midLeft[i] = mid[i];
+        }
+        //处理先序遍历
+        for (int i = 1, j = 0; j < leftNum; i++, j++)
+        {
+            preLeft[j] = pre[i];
+        }
+        t->lChild = buildTree(preLeft, leftNum, midLeft, leftNum);
+    }
+    //生成右子树
+    int rightNum = midLen - 1 - index;
+    if (rightNum > 0)
+    {
+        int midRight[rightNum], preRight[rightNum];
+        //处理中序遍历
+        for (int i = index + 1, j = 0; j < rightNum; i++, j++)
+        {
+            midRight[j] = mid[i];
+        }
+        //处理先序遍历
+        for (int i = leftNum + 1, j = 0; j < rightNum; i++, j++)
+        {
+            preRight[j] = pre[i];
+        }
+        t->rChild = buildTree(preRight, rightNum, midRight, rightNum);
+    }
+
+    return t;
 }
 
 int main()
 {
+    int pre1[6] = {34,23,15,103,98,115};
+    int mid1[6] = {15,23,34,98,103,115};
+    BSTree t1 = buildTree(pre1, 6, mid1, 6);
     int arr[10] = {40, 72, 38, 35, 67, 51, 90, 8, 55, 21};
     BSTree tree = NULL;
     for (int i = 0; i < 10; i++)
@@ -149,6 +341,12 @@ int main()
         tree = insertBst(tree, arr[i]);
     }
     // checkBst(tree);
-    checkTier(tree,40);
-    printf("use tree.\n");
+    // checkTier(tree,40);
+    // int height;
+    // bool result = checkBalance(tree,&height);
+    // printf("checkBalance:%d.\n",result);
+    // findMaxAndMin(t1);
+    // findByK(t1,-1);
+    findIndexK(tree,8);
+    printf("tree applicantion.\n");
 }
