d16e894ccd98d6d47ca5d74a3db6fcbf3a531cc5
lj502766817 <502766817@qq.com>
2023-05-10 14:58:47 +0800

    Create q1.c

---
+++ wangdao/2.2.3/q1.c
@@ -0,0 +1,36 @@
+/**
+ * 从顺序表中删除具有最小值的元素(假设唯一)并由函数返回被删元素的值。空出的位置由最后一个元素填补，若顺序表为空，则显示出错信息并退出运行。
+ */
+# include "../DataStruct.h"
+
+bool delMin(IntSqList list, int *x);
+
+bool delMin(IntSqList list, int *x)
+{
+    if (list->length<1)
+    {
+        return false;
+    }
+    int minIdx = 0;
+    for (int i = 1; i < list->length; i++)
+    {
+        if (list->data[i]<list->data[minIdx])
+        {
+            minIdx = i;
+        }
+    }
+    *x = list->data[minIdx];
+    list->data[minIdx] = list->data[--list->length];
+    return true;
+}
+
+int main()
+{
+    IntSqList seq = initIntSeqList(10,5);
+    printIntSeqList(seq);
+    int x;
+    delMin(seq,&x);
+    printf("min:%d\n",x);
+    printIntSeqList(seq);
+    return 0;
+}
