e9783379a34425c1d2418edaea2ab60671cb893c
lj502766817 <502766817@qq.com>
2023-05-12 18:28:58 +0800

    Update DataStruct.h

--- wangdao/DataStruct.h
+++ wangdao/DataStruct.h
@@ -10,14 +10,24 @@
 #define MAX_SIZE 50
 
 //顺序表
-typedef struct {
+typedef struct 
+{
     int MaxSize,length;
     int *data;
 } IntSeqList,*IntArray;
 
+//链表
+typedef struct IntLinkedList {
+    int data;
+    struct IntLinkedList *next;
+} *IntLinkedArray, IntLinkedList;
+
+//顺序表方法
 IntArray initIntSeqList(int maxData, int len);
 
 IntArray initIntOrderedSeqList(int maxData, int len);
+
+void printIntSeqList(IntArray list);
 
 IntArray initIntOrderedSeqList(int maxData, int len)
 {   
@@ -70,8 +80,6 @@ IntArray initIntSeqList(int maxData, int len)
     return seq;
 }
 
-void printIntSeqList(IntArray list);
-
 void printIntSeqList(IntArray list)
 {
     for (int i = 0; i < list->length; i++)
@@ -81,6 +89,47 @@ void printIntSeqList(IntArray list)
     printf("\n");
 }
 
+//链表方法
+IntLinkedArray initIntLinkedArray(int maxData, int len);
+
+void printIntLinkedArray(IntLinkedArray list);
+
+IntLinkedArray initIntLinkedArray(int maxData, int len)
+{   
+    if (len<1)
+    {
+        return NULL;
+    }
+    
+    srand(time(NULL));
+    IntLinkedArray list = malloc(sizeof(IntLinkedList));
+    list->data = rand() % maxData + 1;
+    list->next = NULL;
+
+    IntLinkedArray current = list;
+    for (int i = 0; i < len; i++)
+    {
+        IntLinkedArray next = malloc(sizeof(IntLinkedList));
+        next->data = rand() % maxData + 1;
+        next->next = NULL;
+        current->next = next;
+        current = next;
+    }
+    
+    return list;
+}
+
+void printIntLinkedArray(IntLinkedArray list)
+{
+    IntLinkedArray p = list;
+    while (p&&p->next)
+    {
+        printf("%d\t",p->data);
+        p=p->next;
+    }
+    printf("\n");
+    
+}
 
 
 #endif //WANGDAO_DATASTRUCT_H
