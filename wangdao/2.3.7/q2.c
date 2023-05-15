c311d9f1ee95e7d1f31d8d977841071e4356b258
lj502766817 <502766817@qq.com>
2023-05-15 17:20:40 +0800

    在带头结点的单链表L中，删除所有值为x的结点，并释放其空间，假设值为x的结点不唯一，试编写算法以实现上述操作。

---
+++ wangdao/2.3.7/q2.c
@@ -0,0 +1,37 @@
+/**
+ * 在带头结点的单链表L中，删除所有值为x的结点，并释放其空间，假设值为x的结点不唯一，试编写算法以实现上述操作。
+ */
+
+#include "../DataStruct.h"
+
+bool delX(IntLinkedList *list, int x);
+
+bool delX(IntLinkedList *list, int x)
+{
+    IntLinkedList *p = list;
+    while (p->next)
+    {
+        if (p->next->data==x)
+        {
+            IntLinkedList *r = p->next;
+            p->next=p->next->next;
+            free(r);
+            printf("remove:%d\n",x);
+        }
+        else
+        {
+            p = p->next;
+        }
+        
+    }
+    return true;
+}
+
+int main()
+{
+    IntLinkedList list = initIntLinkedList(8,10);
+    printIntLinkedArray(&list);
+    delX(&list,5);
+    printIntLinkedArray(&list);
+    return 1;
+}
